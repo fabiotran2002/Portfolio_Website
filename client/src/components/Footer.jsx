85a236d2c044dcead3eda99ecc98b023206ca0e6
Fabio Tran <tranf@it.edu>
2024-11-23 16:09:21 -0500

    Added more contact information to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <div>
+    <div className="border-t-2 border-black">
       <nav className="flex flex-col justify-center items-center mb-6">
         <p className="text-gray-700">Contact Information</p>
         <p className="text-gray-700">
@@ -13,6 +13,16 @@ export default function Footer() {
         <p className="text-gray-700">
           <a href="tel:+16175487329" className="text-blue-500 hover:underline">
             Phone: +1 (617) 548-7329
+          </a>
+        </p>
+        <p className="text-gray-700">
+          <a href="https://www.linkedin.com/in/fabio-tran/" className="text-blue-500 hover:underline">
+            LinkedIn: https://www.linkedin.com/in/fabio-tran/
+          </a>
+        </p>
+        <p className="text-gray-700">
+          <a href="https://github.com/fabiotran2002" className="text-blue-500 hover:underline">
+            GitHub: https://github.com/fabiotran2002
           </a>
         </p>
       </nav>
